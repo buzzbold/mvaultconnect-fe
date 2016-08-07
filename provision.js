938f41e8f50a845d2bc5653f37b4cc5dcd86583a
Steven Ley <steve@buzzbold.com>
2016-08-07 12:26:54 -0500

    qc

--- provision.js
+++ provision.js
@@ -5,7 +5,8 @@ var MVCFE = MVCFE || {};
 
 
 
-MVCFE.provisionAccess: function(framework, data) {
+MVCFE.provisionAccess = function(framework, data) {
+
 
 }
 
@@ -20,52 +21,6 @@ MVCFE.event = {
 
     }
 
-
-
-
-    var testObj = {
-
-__action:, "rc_connect.Campaign_DesignForm.upsertData",
-__campaign:"701R0000000LUTv",
-__form:"00PR0000006fFyrMAE",
-__mode:"view",
-rc_connect__address_city__c:"San Francisco",
-rc_connect__address_country__c:"US",
-rc_connect__address_postal_code__c:"12345",
-rc_connect__address_state__c:"CA",
-rc_connect__address_street_line_1__c:"1234 Count Street",
-rc_connect__address_street_line_2__c:"",
-rc_connect__batch_upload_campaign_matched__c:"701R0000000LUTv",
-rc_connect__contact_1_email__c:"fake@fake.com",
-rc_connect__contact_1_first_name__c:"Test",
-rc_connect__contact_1_last_name__c:"MasterCard",
-rc_connect__event_purchase_giving_amount__c:"",
-rc_connect__event_purchase_giving_paid__c:"false",
-rc_connect__exclude_giving__c:"false",
-rc_connect__giving_close_date__c:"2016-08-04",
-rc_connect__giving_close_date_time__c:"2016-08-04 18:41:02",
-rc_connect__giving_giving_amount__c:"5",
-rc_connect__giving_giving_frequency__c:"Monthly",
-rc_connect__giving_is_sustainer__c:"true",
-rc_connect__giving_paid__c:"true",
-rc_connect__giving_record_type__c:"Donation",
-rc_connect__giving_response_code__c:"000",
-rc_connect__giving_response_message__c:"Approved",
-rc_connect__giving_transaction_id__c:"82918572218942461",
-rc_connect__giving_transaction_type__c:"Payment",
-rc_connect__merchant_name__c:"KQEDVANTIVTEST",
-rc_connect__payment_method_card_expiration_month__c:"12",
-rc_connect__payment_method_card_expiration_year__c:"2023",
-rc_connect__payment_method_card_guid__c:"",
-rc_connect__payment_method_card_holder_name__c:"Test MasterCard",
-rc_connect__payment_method_card_issuer__c:"visa",
-rc_connect__payment_method_card_last_four_digits__c:"0002",
-rc_connect__payment_method_card_number__c:"4100 2003 1000 0002",
-rc_connect__payment_method_card_security_code__c:"123",
-rc_connect__payment_method_payment_type__c:"Charge Card",
-rc_connect__payment_processor__c:"Litle",
-rc_connect__process_events__c:"false"
-};
 
 
 
