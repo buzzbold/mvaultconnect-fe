f80134cc4af6bbd9ed737bf58f6dba1f9d93f354
Steven Ley <steve@buzzbold.com>
2016-08-07 12:39:35 -0500

    qc

--- provision.js
+++ provision.js
@@ -8,26 +8,38 @@ var MVCFE = MVCFE || {};
 MVCFE.provisionAccess = function(framework, data) {
 
 
+
 }
 
 MVCFE.eventData = function (data) {
     this.data = data;
+
+
+
 }
 
 
 MVCFE.event = {
+    typeName : "unknown",
+    handleEvent: function (obj, fn) {
+        this.evalType(obj);
+    },
+    evalType: function(obj) {
 
-    handleEvent: function (obj, type, fn) {
+        try {
+            if (typeof obj !== undefined && obj.__action == "rc_connect.Campaign_DesignForm.upsertData")
+                this.typeName = "rc_connect";
+        } catch (e) {
+
+        }
 
     }
 
-
-
-
-
-    var testImplObject = new MVCFE.eventData(testObj);
-
 }
+
+
+
+
 
 
 
